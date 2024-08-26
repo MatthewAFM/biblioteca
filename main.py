9bb9c2520f7d5b37ef07a6561c1f12b20aa71fe1
MatthewAFM <109297675+MatthewAFM@users.noreply.github.com>
2024-08-25 23:32:43 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#En esta seccion seria para organizar todo el main, pero creo que no logre realizar de manera correcta la realacion
+
+from Lector import lector
+from libro import libro
+
+class libreria:
+    def __str__(self):
+        print("Bienvenido a la libreria Matthew Flores")
+        print("La direccion de la libreria es: 1ra calle B zona 16 colonia patito")
+        return f"Nuestro catologo es: {libro}"
+        
+       
